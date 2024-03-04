cb884d692d3afe4793f5c7a939465a685490ca78
Siqi Li <1990lisiqi@gmail.com>
2024-03-04 18:11:51 -0500

    Update download_sst_ostia_0p05.py

--- download_sst_ostia_0p05.py
+++ download_sst_ostia_0p05.py
@@ -29,8 +29,8 @@ from datetime import datetime, timedelta
 import copernicusmarine
 
 # Settings
-user = "sli12"
-pswd = "123qweASDF"
+user = ""
+pswd = ""
 output_directory = './'
 
 prefix = "SST_PSTIA_0p05"
